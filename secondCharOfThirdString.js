596f8a7683abddb7527169be95d6d6d2eafc1b44
TreySchneider95 <tschneid95@gmail.com>
2022-09-22 14:35:37 -0500

    fixed last problem

--- secondCharOfThirdString.js
+++ secondCharOfThirdString.js
@@ -2,4 +2,4 @@ const prompt = require('prompt-sync')({sigint: true});
 
 let userArray = JSON.parse(prompt("Enter array: "))
 
-typeof userArray[0] === "string" && userArray[0].length >= 2 ? console.log(userArray[0][1]):console.log("Error: first item is either not a string or not long enough")
+typeof userArray[0] === "string" && userArray[3].length >= 2 ? console.log(userArray[3][1]):console.log("Error: first item is either not a string or not long enough")
